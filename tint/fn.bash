d482012f31f6ec6f6d95509bae203d58baf276b9
George Georgalis <george@galis.org>
2022-01-01 21:52:22 -0800

    artifact of development, and a bug, removed.

--- tint/fn.bash
+++ tint/fn.bash
@@ -6,7 +6,7 @@ color_dark () { # numerator (arg1) and denominator (arg2) of a number 0<x<1
     local a n="$1" d="$2"
     shift 2
     # of seq 400 $(dc -e "7k 1 $n / 400 * p") 800
-    set  $@ $(./nm2rgb.py $(dc -e "7k 1 $d / $n * 2 - _400 * p") )
+    set $(./nm2rgb.py $(dc -e "7k 1 $d / $n * 2 - _400 * p") )
     # set the background, with tinted modifications
     printf '\033[48;2;%d;%d;%dm' $(( $1 *1000 *47/100 /1000 )) $(( $2 *1000 *33/100 /1000 )) $(( $3 *1000 *59/100 /1000 ))
     }
